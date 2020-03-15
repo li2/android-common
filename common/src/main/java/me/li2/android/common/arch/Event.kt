44c03a018adc5d6dd03f6759fae35a5275ebd1f1
Weiyi Li <weiyi.just2@gmail.com>
2020-03-15 17:15:31 +1300

    Added Resource Added SingleLiveEvent

--- common/src/main/java/me/li2/android/common/arch/Event.kt
+++ common/src/main/java/me/li2/android/common/arch/Event.kt
@@ -1,11 +1,11 @@
-package me.li2.android.common.event
+package me.li2.android.common.arch
 
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 
 /**
- * Used as a wrapper for data that is exposed via a LiveData that represents an event.
+ * A wrapper for data that is exposed via a LiveData that represents an event.
  * https://medium.com/androiddevelopers/livedata-with-snackbar-navigation-and-other-events-the-singleliveevent-case-ac2622673150
  */
 open class Event<out T>(private val content: T) {
@@ -35,7 +35,7 @@ open class Event<out T>(private val content: T) {
  * An Observer for [Event]s, simplifying the pattern of checking if the Event's content has already been handled.
  *
  * @param onEventUnhandledContent is *ONLY* called if the [Event]'s contents has not been handled.
- * @see <a href="https://medium.com/androiddevelopers/livedata-with-snackbar-navigation-and-other-events-the-singleliveevent-case-ac2622673150">LiveData with SnackBar, Navigation and other events (the SingleLiveEvent case)</a>
+ * @see <a href="https://medium.com/androiddevelopers/livedata-with-snackbar-navigation-and-other-events-the-singleliveevent-case-ac2622673150">LiveData with SnackBar, Navigation and other events</a>
  * @see <a href="https://gist.github.com/JoseAlcerreca/e0bba240d9b3cffa258777f12e5c0ae9">EventObserver.kt</a>
  */
 fun <T : Any, L : LiveData<Event<T>>> Fragment.observeEventOnView(
---
+++ common/src/main/java/me/li2/android/common/arch/LiveDataExt.kt
@@ -0,0 +1,19 @@
+package me.li2.android.common.arch
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+
+fun <T> MutableLiveData<Resource<T>>.postSuccess(data: T) =
+        postValue(Resource.success(data))
+
+fun <T> MutableLiveData<Resource<T>>.postLoading() =
+        postValue(Resource.loading(value?.data))
+
+fun <T> MutableLiveData<Resource<T>>.postError(exception: Exception? = null) =
+        postValue(Resource.error(exception))
+
+fun <T : Any, L : LiveData<T>> Fragment.observeOnView(liveData: L, body: (T) -> Unit) {
+    liveData.observe(viewLifecycleOwner, Observer(body))
+}
