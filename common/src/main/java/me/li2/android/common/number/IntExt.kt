6c65a8fcabe855777cb44fbdf93b64c0960fedc6
Weiyi Li <weiyi.just2@gmail.com>
2020-05-03 17:21:53 +1200

    Added dimension dp and px converter

--- common/src/main/java/me/li2/android/common/number/IntExt.kt
+++ common/src/main/java/me/li2/android/common/number/IntExt.kt
@@ -3,6 +3,17 @@
  * https://github.com/li2
  */
 @file:Suppress("unused")
+
 package me.li2.android.common.number
 
+import android.content.Context
+import androidx.annotation.Dimension
+import androidx.annotation.Px
+
 fun Int?.orZero() = this ?: 0
+
+@Px
+fun Int.dpToPx(context: Context): Int = (this * context.resources.displayMetrics.density).toInt()
+
+@Dimension(unit = Dimension.DP)
+fun Int.pxToDp(context: Context): Int = (this / context.resources.displayMetrics.density).toInt()
