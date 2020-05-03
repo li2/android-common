ba1c057a25124d883ded6c035e911d16ec488d8a
Weiyi Li <weiyi.just2@gmail.com>
2020-05-03 17:49:22 +1200

    LiveData isLoading, isError, isIdle extension functions

--- common/src/main/java/me/li2/android/common/arch/LiveDataExt.kt
+++ common/src/main/java/me/li2/android/common/arch/LiveDataExt.kt
@@ -10,6 +10,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import me.li2.android.common.arch.Resource.Status.ERROR
+import me.li2.android.common.arch.Resource.Status.LOADING
 
 fun <T> MutableLiveData<Resource<T>>.postSuccess(data: T) =
         postValue(Resource.success(data))
@@ -19,6 +21,19 @@ fun <T> MutableLiveData<Resource<T>>.postLoading() =
 
 fun <T> MutableLiveData<Resource<T>>.postError(exception: Exception? = null) =
         postValue(Resource.error(exception, value?.data))
+
+fun<T> MutableLiveData<Resource<T>>.isIdle(): Boolean {
+    val status = value?.status
+    return status != LOADING && status != ERROR
+}
+
+fun<T> MutableLiveData<Resource<T>>.isLoading(): Boolean {
+    return value?.status == LOADING
+}
+
+fun<T> MutableLiveData<Resource<T>>.isError(): Boolean {
+    return value?.status == ERROR
+}
 
 fun <T : Any, L : LiveData<T>> Fragment.observeOnView(liveData: L, body: (T) -> Unit) {
     liveData.observe(viewLifecycleOwner, Observer(body))
