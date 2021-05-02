135b275bbe9b54efdd66916b2fb3a38a1321a38f
Weiyi Li <weiyi.just2@gmail.com>
2021-05-02 17:06:03 +1200

    Refactor RxSdcardState

--- app/src/main/java/me/li2/android/commonsample/MainActivity.kt
+++ app/src/main/java/me/li2/android/commonsample/MainActivity.kt
@@ -3,29 +3,21 @@ package me.li2.android.commonsample
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.rxkotlin.plusAssign
 import me.li2.android.common.framework.SimUtils
-import me.li2.android.common.rx.onSdcardStateChanges
+import me.li2.android.common.rx.sdcardStateChanges
+import me.li2.android.common.rx.subscribeOnLifecycle
 import timber.log.Timber.d
 
 class MainActivity : AppCompatActivity() {
-
-    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.resultTextView).text = SimUtils.getSimInfo(this)
-        
-        compositeDisposable += onSdcardStateChanges().subscribe {
+
+        sdcardStateChanges().subscribeOnLifecycle(lifecycle) {
             d("sdcard state changed: $it")
         }
-    }
-
-    override fun onDestroy() {
-        compositeDisposable.dispose()
-        super.onDestroy()
     }
 }
--- common/src/main/java/me/li2/android/common/rx/RxSdcardState.kt
+++ common/src/main/java/me/li2/android/common/rx/RxSdcardState.kt
@@ -1,20 +1,16 @@
 package me.li2.android.common.rx
 
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import io.reactivex.Emitter
 import io.reactivex.Observable
-import io.reactivex.ObservableEmitter
-import io.reactivex.ObservableOnSubscribe
-import io.reactivex.disposables.Disposable
 
 /*
  * @created: 01 May 2021.
  * @author: Weiyi Li
  */
-enum class SdcardState(val action: String) {
+enum class SdcardState(val action: String?) {
+    UNKNOWN(null),
     MOUNTED(Intent.ACTION_MEDIA_MOUNTED),
     EJECT(Intent.ACTION_MEDIA_EJECT),
     UNMOUNTED(Intent.ACTION_MEDIA_UNMOUNTED),
@@ -25,54 +21,20 @@ enum class SdcardState(val action: String) {
     companion object {
         private val map = enumValues<SdcardState>().associateBy(SdcardState::action)
 
-        val intentActions: List<String> = enumValues<SdcardState>().map { it.action }
+        val intentActions: List<String> = enumValues<SdcardState>().mapNotNull { it.action }
 
-        fun from(action: String?): SdcardState? = map[action]
+        fun from(action: String?): SdcardState = map[action] ?: UNKNOWN
     }
 }
 
 /**
  * Create an observable which emits the state of sdcard
  */
-fun Context.onSdcardStateChanges(): Observable<SdcardState> =
-    Observable.defer { Observable.create(SdcardStateChangeObservable(this)) }
-
-
-private class SdcardStateChangeObservable(private val context: Context) :
-    ObservableOnSubscribe<SdcardState>,
-    Disposable {
-
-    private var intentFilter: IntentFilter = IntentFilter()
+fun Context.sdcardStateChanges(): Observable<SdcardState> {
+    val intentFilter = IntentFilter()
         .apply {
             SdcardState.intentActions.forEach { action -> addAction(action) }
             addDataScheme("file")
         }
-    private var broadcastReceiver: BroadcastReceiver?
-    private lateinit var emitter: Emitter<in SdcardState>
-
-    init {
-        broadcastReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                if (::emitter.isInitialized) {
-                    SdcardState.from(intent.action)?.let { sdcardState ->
-                        emitter.onNext(sdcardState)
-                    }
-                }
-            }
-        }
-    }
-
-    override fun subscribe(emitter: ObservableEmitter<SdcardState>) {
-        this.emitter = emitter
-        context.registerReceiver(broadcastReceiver, intentFilter)
-    }
-
-    override fun dispose() {
-        if (broadcastReceiver != null) {
-            context.unregisterReceiver(broadcastReceiver)
-        }
-        broadcastReceiver = null
-    }
-
-    override fun isDisposed(): Boolean = broadcastReceiver != null
+    return onBroadcast(intentFilter).map { intent -> SdcardState.from(intent.action) }
 }
